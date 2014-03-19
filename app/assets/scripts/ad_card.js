af586c7fa0d1c1404c8ef6e0bcd1dc16b9f51a38
Scott Munson <smunson@cinema6.com>
2014-03-19 10:05:12 -0400

    new ad card files

---
+++ app/assets/scripts/ad_card.js
@@ -0,0 +1,14 @@
+(function(){
+	'use strict';
+
+	angular.module('c6.rumble')
+		.directive('adCard', [ '$log', 
+		function			 ( '$log' ) {
+			$log = $log.context('<ad-card>');
+
+			function fnLink(scope, $element, $attr) {
+
+			}
+		}]);
+
+}());
