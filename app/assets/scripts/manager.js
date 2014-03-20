0d15d0003a63d24c4e2b51e7460281be69edb192
Joshua Minzner <josh@minzner.org>
2014-03-20 10:28:47 -0400

    Add editor state. Upgrade c6ui

--- app/assets/scripts/manager.js
+++ app/assets/scripts/manager.js
@@ -2,8 +2,12 @@
     'use strict';
 
     angular.module('c6.mrmaker')
-        .controller('ManagerController', ['cModel',
-        function                         ( cModel ) {
+        .controller('ManagerController', ['cModel','c6State',
+        function                         ( cModel , c6State ) {
             this.model = cModel;
+
+            this.edit = function(minireel) {
+                c6State.transitionTo('editor', { id: minireel.id });
+            };
         }]);
 }());
---
+++ test/spec/editor.state.ut.js
@@ -0,0 +1,93 @@
+(function() {
+    'use strict';
+
+    define(['app'], function() {
+        describe('EditorState', function() {
+            var $injector,
+                EditorState,
+                $rootScope,
+                $q,
+                cinema6,
+                c6State;
+
+            var c6StateParams,
+                MiniReelService;
+
+            var experience = {
+                    id: 'e-9990920583a712'
+                },
+                minireel = {
+                    id: 'e-9990920583a712',
+                    processed: true
+                };
+
+            beforeEach(function() {
+                module('c6.state', function($provide) {
+                    $provide.value('c6StateParams', {});
+                });
+
+                module('c6.mrmaker', function($provide) {
+                    $provide.value('MiniReelService', {
+                        open: jasmine.createSpy('MiniReelService.open()')
+                            .and.returnValue(minireel)
+                    });
+                });
+
+                inject(function(_$injector_) {
+                    $injector = _$injector_;
+
+                    $rootScope = $injector.get('$rootScope');
+                    $q = $injector.get('$q');
+                    cinema6 = $injector.get('cinema6');
+                    c6State = $injector.get('c6State');
+
+                    c6StateParams = $injector.get('c6StateParams');
+                    MiniReelService = $injector.get('MiniReelService');
+
+                    EditorState = c6State.get('editor');
+                });
+            });
+
+            it('should exist', function() {
+                expect(EditorState).toEqual(jasmine.any(Object));
+            });
+
+            describe('model()', function() {
+                var result,
+                    success;
+
+                beforeEach(function() {
+                    success = jasmine.createSpy('model() success');
+
+                    spyOn(cinema6.db, 'find')
+                        .and.callFake(function() {
+                            return $q.when(experience);
+                        });
+
+                    c6StateParams.id = 'e-9990920583a712';
+
+                    $rootScope.$apply(function() {
+                        result = $injector.invoke(EditorState.model);
+                        result.then(success);
+                    });
+                });
+
+                it('should return a promise', function() {
+                    expect(result.then).toEqual(jasmine.any(Function));
+                });
+
+                it('should find the experience based on the state params', function() {
+                    expect(cinema6.db.find).toHaveBeenCalledWith('experience', c6StateParams.id);
+                });
+
+                it('should "open" the minireel for editing', function() {
+                    expect(MiniReelService.open).toHaveBeenCalledWith(experience);
+                });
+
+                it('should resolve to the transpiled minireel', function() {
+                    expect(success).toHaveBeenCalledWith(minireel);
+                });
+            });
+        });
+    });
+}());
