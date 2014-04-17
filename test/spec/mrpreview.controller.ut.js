f83d65ce55d4d1461327632c8335671a8180c27a
Scott Munson <scott@thinkcontra.com>
2014-04-17 09:08:39 -0400

    testing...

--- test/spec/mrpreview.controller.ut.js
+++ test/spec/mrpreview.controller.ut.js
@@ -2,69 +2,84 @@
     'use strict';
 
     define(['editor'], function() {
-        describe('MRPreviewController', function() {
+        describe('PreviewController', function() {
             var $rootScope,
                 $scope,
                 $controller,
                 MiniReelService,
-                postMessage,
-                session,
                 c6BrowserInfo,
-                MRPreviewController;
+                PreviewController;
+
+            var session, postMessage;
 
             beforeEach(function() {
                 c6BrowserInfo = {
                     profile: {}
                 };
 
+                // session = {
+                //     on: jasmine.createSpy('session.on()'),
+                //     ping: jasmine.createSpy('session.ping()')
+                // };
+
                 session = {
-                    on: jasmine.createSpy('session.on()')
+                    on: function(){},
+                    ping: function(){}
                 };
 
                 module('c6.ui', function($provide) {
                     $provide.value('c6BroswerInfo',c6BrowserInfo);
+                    $provide.service('postMessage',{
+                        createSession: jasmine.createSpy('postMessage.createSession()')
+                            .andReturn(session)
+                    });
                 });
-                module('c6.mrmaker');
+
+                module('c6.mrmaker', function($provide) {
+                    $provide.service('MiniReelService',{
+                        convertForPlayer: jasmine.createSpy('MiniReelService.convertForPlayer()')
+                    });
+                });
 
                 inject(function($injector) {
                     $rootScope = $injector.get('$rootScope');
                     $controller = $injector.get('$controller');
-                    $scope = $rootScope.$new();
                     MiniReelService = $injector.get('MiniReelService');
                     postMessage = $injector.get('postMessage');
 
-                    MRPreviewController = $controller('MRPreviewController', { $scope: $scope });
+                    $scope = $rootScope.$new();
+
+                    PreviewController = $controller('PreviewController', { $scope: $scope });
                 });
 
             });
 
-            xdescribe('initialization', function() {
+            describe('initialization', function() {
                 it('should exist', function() {
-                    expect(MRPreviewController).toEqual(jasmine.any(Object));
+                    expect(PreviewController).toEqual(jasmine.any(Object));
                 });
 
                 it('should set the default mode to full', function() {
-                    expect(MRPreviewController.mode).toBe('full');
+                    expect(PreviewController.device).toBe('desktop');
                 });
             });
 
-            xdescribe('$scope listeners', function() {
+            describe('$scope listeners', function() {
                 describe('mrPreview:initExperience', function() {
-                    var exp, iframe;
+                    var exp,
+                        iframe;
 
                     beforeEach(function() {
                         exp = {};
                         iframe = {
                             prop: jasmine.createSpy('iframe.prop()')
                         };
-                        MiniReelService.preview = jasmine.createSpy('MiniReelService.preview()');
-                        spyOn(postMessage, 'createSession').andReturn(session);
                     });
 
                     it('should do stuff', function() {
                         $scope.$emit('mrPreview:initExperience', exp, iframe);
                         expect(iframe.prop).toHaveBeenCalled();
-                        expect(MiniReelService.preview).toHaveBeenCalled();
+                        expect(MiniReelService.convertForPlayer).toHaveBeenCalled();
                         expect(postMessage.createSession).toHaveBeenCalled();
                         expect(session.on).toHaveBeenCalled();
                     });
