ae715b3f6f17b22d2674f920ce1366d68e5e28ae
Joshua Minzner <josh@minzner.org>
2014-07-15 13:07:45 -0400

    Add click listeners for all elements with the c6js-start class.

--- c6Content/splash/splash.js
+++ c6Content/splash/splash.js
@@ -2,14 +2,16 @@ module.exports = function(c6, settings, splash) {
     'use strict';
 
     var loader = splash.querySelectorAll('.c6js-loader')[0],
-        start = splash.querySelectorAll('.c6js-start')[0];
+        starts = Array.prototype.slice.call(splash.querySelectorAll('.c6js-start'));
 
-    start.addEventListener('click', function() {
-        if (loader) {
-            loader.style.display = '';
-        }
-        c6.loadExperience(settings);
-    }, false);
+    starts.forEach(function(start) {
+        start.addEventListener('click', function() {
+            if (loader) {
+                loader.style.display = '';
+            }
+            c6.loadExperience(settings);
+        }, false);
+    });
 
     return {
         didHide: loader ? function() {
