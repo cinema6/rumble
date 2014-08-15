26378fd1f96c33e10140775180ca2f174e09d8c0
Joshua Minzner <josh@minzner.org>
2014-08-15 16:30:04 -0400

    lightbox- recalculate the size/position of the paginator when navigating to/from a text card.

--- test/spec/text_card.controller.ut.js
+++ test/spec/text_card.controller.ut.js
@@ -1,20 +1,22 @@
-define(['cards/text'], function(textCardModule) {
+define(['app'], function(appModule) {
     'use strict';
 
     describe('TextCardController', function() {
         var $rootScope,
             $scope,
             $controller,
+            c6AppData,
             ModuleService,
             AdTechService,
             TextCardCtrl;
 
         beforeEach(function() {
-            module(textCardModule.name);
+            module(appModule.name);
 
             inject(function($injector) {
                 $rootScope = $injector.get('$rootScope');
                 $controller = $injector.get('$controller');
+                c6AppData = $injector.get('c6AppData');
                 ModuleService = $injector.get('ModuleService');
                 AdTechService = $injector.get('AdTechService');
 
@@ -29,6 +31,7 @@ define(['cards/text'], function(textCardModule) {
             spyOn(ModuleService, 'hasModule').andCallThrough();
             spyOn(AdTechService, 'loadAd');
 
+            spyOn($rootScope, '$broadcast').andCallThrough();
             $scope.$apply(function() {
                 TextCardCtrl = $controller('TextCardController', { $scope: $scope });
             });
@@ -52,6 +55,73 @@ define(['cards/text'], function(textCardModule) {
 
         describe('$watchers', function() {
             describe('active', function() {
+                describe('if the mode is "lightbox"', function() {
+                    beforeEach(function() {
+                        c6AppData.mode = 'lightbox';
+                    });
+
+                    describe('on initializaion', function() {
+                        beforeEach(function() {
+                            $rootScope.$broadcast = jasmine.createSpy('$broadcast');
+                            $scope.$apply(function() {
+                                TextCardCtrl = $controller('TextCardController', { $scope: $scope });
+                            });
+                        });
+
+                        it('should not $broadcast resize', function() {
+                            expect($rootScope.$broadcast).not.toHaveBeenCalled();
+                        });
+                    });
+
+                    describe('when true', function() {
+                        beforeEach(function() {
+                            $scope.$apply(function() {
+                                $scope.active = true;
+                            });
+                        });
+
+                        it('should $broadcast resize', function() {
+                            expect($rootScope.$broadcast).toHaveBeenCalledWith('resize');
+                        });
+                    });
+
+                    describe('when false', function() {
+                        beforeEach(function() {
+                            [true, false].forEach(function(bool) {
+                                $scope.$apply(function() {
+                                    $scope.active = bool;
+                                });
+                            });
+                        });
+
+                        it('should $broadcast resize', function() {
+                            expect($rootScope.$broadcast).toHaveBeenCalledWith('resize');
+                        });
+                    });
+                });
+
+                ['lightbox-ads', 'light', 'full'].forEach(function(mode) {
+                    describe('if the mode is "' + mode + '"', function() {
+                        beforeEach(function() {
+                            c6AppData.mode = mode;
+                        });
+
+                        describe('when false', function() {
+                            beforeEach(function() {
+                                [true, false].forEach(function(bool) {
+                                    $scope.$apply(function() {
+                                        $scope.active = bool;
+                                    });
+                                });
+                            });
+
+                            it('should not $broadcast resize', function() {
+                                expect($rootScope.$broadcast).not.toHaveBeenCalledWith('resize');
+                            });
+                        });
+                    });
+                });
+
                 describe('without a displayAd', function() {
                     beforeEach(function() {
                         $scope.config.modules.length = 0;
