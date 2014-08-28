577513a5d19cd0da69bc826e90f952119cf517e9
Joshua Minzner <josh@minzner.org>
2014-08-28 09:22:46 -0400

    Refactor the InflectorService.

--- app/assets/scripts/services.js
+++ app/assets/scripts/services.js
@@ -698,35 +698,15 @@ function( angular , c6ui , adtech , c6Defines  ) {
             var exceptions = [];
 
             this.pluralize = function(word) {
-                var plural = (function() {
-                    var result;
-
-                    exceptions.some(function(exception) {
-                        if (exception.singular === word) {
-                            return !!(result = exception.plural);
-                        }
-                    });
-
-                    return result;
-                }());
-
-                return plural || (word + 's');
+                return exceptions.reduce(function(result, exception) {
+                    return exception.singular === word ? exception.plural : result;
+                }, word + 's');
             };
 
             this.singularize = function(word) {
-                var singular = (function() {
-                    var result;
-
-                    exceptions.some(function(exception) {
-                        if (exception.plural === word) {
-                            return !!(result = exception.singular);
-                        }
-                    });
-
-                    return result;
-                }());
-
-                return singular || word.replace(/s$/, '');
+                return exceptions.reduce(function(result, exception) {
+                    return exception.plural === word ? exception.singular : result;
+                }, word.replace(/s$/, ''));
             };
 
             this.capitalize = function(word) {
@@ -734,52 +714,16 @@ function( angular , c6ui , adtech , c6Defines  ) {
             };
 
             this.getWords = function(string) {
-                var result = [],
-                    word, character, index, length;
-
-                function isDelimiter(char) {
-                    return !!char.match(/-|_|[A-Z]| /);
-                }
-
-                function isLetter(char) {
-                    return !!char.match(/[A-Za-z]/);
-                }
-
-                function pushWord(word) {
-                    result.push(word.toLowerCase());
-                }
-
                 if (angular.isArray(string)) { return string; }
 
-                word = string.charAt(0);
-                length = string.length;
-
-                for (index = 1; index < length; index++) {
-                    character = string.charAt(index);
-
-                    if (isDelimiter(character) && word) {
-                        pushWord(word);
-                        word = isLetter(character) ? character : '';
-
-                        continue;
-                    }
-
-                    word += character;
-                }
-
-                pushWord(word);
-
-                return result;
+                return string.match(/[A-Z]?[^-|_| |A-Z]+|[A-Z]/g)
+                    .map(function(word) { return word.toLowerCase(); });
             };
 
             this.toCamelCase = function(words) {
-                var self = this;
-
                 return this.getWords(words).map(function(word, index) {
-                    if (index === 0) { return word; }
-
-                    return self.capitalize(word);
-                }).join('');
+                    return index ? this.capitalize(word) : word;
+                }, this).join('');
             };
 
             this.toConstructorCase = function(words) {
