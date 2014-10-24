d2a864b84d35588b55f015cb0ff53d22c8876f7a
Joshua Minzner <josh@minzner.org>
2014-10-24 15:35:10 -0400

    Upgrade c6ui.

--- app/assets/scripts/main.js
+++ app/assets/scripts/main.js
@@ -31,8 +31,8 @@
             angularAnimate: libUrl('angular/v1.2.12-0-g5cc5cc1/angular-animate.min'),
             angularSanitize: libUrl('angular/v1.2.12-0-g5cc5cc1/angular-sanitize.min'),
             angularTouch: libUrl('angular/v1.2.12-0-g5cc5cc1/angular-touch.min'),
-            c6uilib: libUrl('c6ui/v3.1.2-0-ga8233d1/c6uilib.min'),
-            c6log: libUrl('c6ui/v3.1.2-0-ga8233d1/c6log.min'),
+            c6uilib: libUrl('c6ui/v3.1.3-0-gd8d2341/c6uilib.min'),
+            c6log: libUrl('c6ui/v3.1.3-0-gd8d2341/c6log.min'),
             adtech: [
                 protocol + '//aka-cdn.adtechus.com/dt/common/DAC',
                 'backup/shmadshteck'
