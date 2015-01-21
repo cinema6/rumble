68b73ff3b5c5ff01c5a15f9b61c3a285873271d6
Scott Munson <smunson@cinema6.com>
2015-01-21 15:00:17 -0500

    update c6ui: 3.6.5

--- test/test-main.js
+++ test/test-main.js
@@ -38,8 +38,8 @@
                 angularSanitize: libUrl('angular/v1.2.12-0-g5cc5cc1/angular-sanitize'),
                 angularTouch: libUrl('angular/v1.2.12-0-g5cc5cc1/angular-touch'),
                 angularMocks: libUrl('angular/v1.2.12-0-g5cc5cc1/angular-mocks'),
-                c6uilib: libUrl('c6ui/v3.6.3-0-g08236e8/c6uilib'),
-                c6log: libUrl('c6ui/v3.6.3-0-g08236e8/c6log'),
+                c6uilib: libUrl('c6ui/v3.6.5-0-g1f59a0b/c6uilib'),
+                c6log: libUrl('c6ui/v3.6.5-0-g1f59a0b/c6log'),
                 adtech: 'http://aka-cdn.adtechus.com/dt/common/DAC',
                 templates   : '/base/.tmp/templates'
             },
